63deb3a38071cffafced45d62ea07c158eeae451
farron_cat <1627173301@qq.com>
2024-04-16 19:53:22 +0800

    update

--- bin/autoDeploy.bat
+++
@@ -1,8 +0,0 @@
-call pnpm docs:build
-cd docs/.vitepress/dist
-
-git init
-git add -A
-git commit -m "auto construct blog"
-
-git push -f https://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@github.com/farron-cat/farron-cat.github.io.git main:gh-pages
